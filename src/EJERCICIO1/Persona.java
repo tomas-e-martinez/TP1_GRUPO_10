26404c823c26831ea4d50a8770af011435c11e00
Tomás Martínez <tomas.emart@gmail.com>
2025-08-22 21:58:55 -0300

    feat: add EJERCICIO1 package and Persona class with properties

---
+++ src/EJERCICIO1/Persona.java
@@ -0,0 +1,14 @@
+package EJERCICIO1;
+
+import java.time.LocalDate;
+
+public class Persona {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String genero;
+	private String direccion;
+	private String telefono;
+	private String email;
+}
