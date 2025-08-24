2372f9fcce560ce0e4bd8c2cabd4798c2a68a1f2
Ramiro <ramiroadal626@gmail.com>
2025-08-23 22:08:14 -0300

    clase empleado

---
+++ src/EJERCICIO1/Empleado.java
@@ -0,0 +1,39 @@
+package EJERCICIO1;
+
+public class Empleado extends Persona {
+
+	private final int legajo;
+	private String puesto;
+	private static int contadorLegajo = 1000;
+	
+	public Empleado() {
+		super();
+		contadorLegajo++;
+		this.legajo = contadorLegajo;
+		this.puesto = "sin puesto";
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Legajo del empleado:" + legajo + ", Puesto del empleado:" + puesto;
+	}
+
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public static int getContadorLegajo() {
+		return contadorLegajo;
+	}
+	 
+}
