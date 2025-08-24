777518e3d5d9411af12e198eec95fa51f133e636
Ramiro <ramiroadal626@gmail.com>
2025-08-24 18:11:29 -0300

    print de legajo e inicialización de 5 objetos en Principal.java

--- src/EJERCICIO1/Empleado.java
+++ src/EJERCICIO1/Empleado.java
@@ -15,16 +15,18 @@ public class Empleado extends Persona {
 		this.puesto = "sin puesto";
 	}
 
-	public Empleado(String dni, 
-			String nombre, 
-			String apellido, 
-			LocalDate fechaNacimiento,
-            String genero, String direccion, String telefono, String email,
-            String puesto) {
-			super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
-			this.legajo = contadorLegajo;
-			contadorLegajo++;
-			this.puesto = puesto;
+	public Empleado(
+				String dni, 
+				String nombre, 
+				String apellido, 
+				LocalDate fechaNacimiento,
+            	String genero, String direccion, String telefono, String email,
+            	String puesto)
+	{
+		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		this.legajo = contadorLegajo;
+		contadorLegajo++;
+		this.puesto = puesto;
 	}
 
 	public static int devuelveProximoLegajo() {
